7e5ede8e35b6af09683da98315e6d240a3a12d34
William Emerison Six <billsix@gmail.com>
2015-03-19 23:14:32 -0400

    Build without using relative pathnames.  Use BUG_HOME environment variable instead.
    Build as a shared library (still need to build as a 'load'-able object for use with gsi.
    
    Updated demo to use the shared library

--- demo/demo.scm
+++ demo/demo.scm
@@ -4,25 +4,36 @@
 
 (include "../src/lang#.scm")
 (include "../src/lang-macros.scm")
-(include "../src/collections/list#.scm")
 
+(at-both-times
+ (begin
+   (my-include "lang#.scm")
+   (my-include "collections/list#.scm")))
 
 (at-compile-time
  (begin
-   (include "../src/lang#.scm")
-   (include "../src/lang.scm")
-   (include "../src/lang-macros.scm")
-   (include "../src/collections/list#.scm")))
-   ;;(include "../src/collections/list.scm")))
+   (my-include "lang.scm")
+   (my-include "lang-macros.scm")
+   (my-include "collections/list.scm")))
 
-(with-tests
- (define (bar a)
-   (aif a
-	(list it it)))
- (equal? (bar #f) #f)
- (equal? (bar 3) '(3 3)))
 
-(define (main)
-  (pp (bar "demo ")))
 
-(main)
+(print "Executing (pp (identity 5)) results in -> ")
+(pp (identity 5))
+
+(print "Executing (aif 5 3) results in -> ")
+(pp (aif 5 3))
+
+(print "Executing (first '(1 2 3 4 5 6))) results in -> ")
+(pp (first '(1 2 3 4 5 6)))
+
+(print "Executing (last '(1 2 3 4 5 6))) results in -> ")
+(pp (last '(1 2 3 4 5 6)))
+
+(print "Executing (but-first '(1 2 3 4 5 6))) results in -> ")
+(pp (but-first '(1 2 3 4 5 6)))
+
+(print "Executing (reverse! (list 1 2 3 4))) results in -> ")
+(pp (reverse! (list 1 2 3 4)))
+
+
