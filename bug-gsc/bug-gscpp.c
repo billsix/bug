84b3b38ec1e103ebe7546feea940b662d9f87d3f
William Emerison Six <billsix@gmail.com>
2015-05-19 19:47:06 +0000

    Added an interpreter, which interactively forwards the output of bug-gscpp to gsi, thus allowing interactive use of my syntax extensions.
    Fixed bug in bug-gscpp, in which it assumed that calls
    to 'read' would block, which caused problems when piped
    to a program like gsi.

--- bug-gsc/bug-gscpp.c
+++ bug-gsc/bug-gscpp.c
@@ -36,44 +36,76 @@ ssize_t xwrite(const void *buf, size_t count)
 }
 
 
+ssize_t xread(int fd, void *buf, size_t count)
+{
+  ssize_t bytesRead;
+ readFromInput:
+  bytesRead = read(fd, buf, count);
+  if(bytesRead == -1)
+    {
+      if(errno == EINTR)
+	goto readFromInput;
+      if (errno == EAGAIN)
+	goto readFromInput;
+      else
+	{
+	  puts ("Unhandled read error");
+	  exit(1);
+	}
+
+    }
+  return bytesRead;
+}
+
 int main(int argc, char** argv)
 {
-  if(argc < 2)
+  // either read from stdin and out,
+  // or from files
+  if(argc != 1 && argc != 3)
     {
+      puts("Usage: bug-gscpp , or");
       puts("Usage: bug-gscpp inputfile outputfile");
       exit(1);
     }
-  // open input file
-  {
-    char* programName = argv[1];  
-    inputfd = open(programName, O_RDONLY);
-    if(inputfd == -1)
+  // if no args passed, read from stdin and stdout
+  if(argc == 1)
+    {
+      inputfd = STDIN_FILENO;
+      outputfd = STDOUT_FILENO;
+    }
+  // otherwise open the input and output files
+  else
+    {
+      // open input file
       {
-	// TODO:  Handle errnos
-	printf("Error: unable to open input file %s, errno %d \n",programName, errno);
-	exit(1);
+	char* programName = argv[1];
+	inputfd = open(programName, O_RDONLY);
+	if(inputfd == -1)
+	  {
+	    // TODO:  Handle errnos
+	    printf("Error: unable to open input file %s, errno %d \n",programName, errno);
+	    exit(1);
+	  }
       }
-  }
-  // open output file
-  {
-    char* outputFileName = argv[2];
-    outputfd = creat(outputFileName, S_IRWXU);
-    if(outputfd == -1)
+      // open output file
       {
-	// TODO:  Handle errnos
-	printf("Error: unable to open output file %s, errno %d \n",outputFileName, errno);
-	exit(1);
+	char* outputFileName = argv[2];
+	outputfd = creat(outputFileName, S_IRWXU);
+	if(outputfd == -1)
+	  {
+	    // TODO:  Handle errnos
+	    printf("Error: unable to open output file %s, errno %d \n",outputFileName, errno);
+	    exit(1);
+	  }
       }
-  }
+    }
   // read the input file into a buffer, translate special characters
   // like "[]{}||" into valid scheme code, write to output file
   {
     char buf[SIZE_OF_INPUT_BUFFER];
     ssize_t bytesRead = 0;
-    // TODO handle read errors
 
-    
-    while(bytesRead = read(inputfd, buf, SIZE_OF_INPUT_BUFFER), 
+    while(bytesRead = xread(inputfd, buf, SIZE_OF_INPUT_BUFFER),
 	  bytesRead > 0)
       {
 	for(int i = 0; i < bytesRead; i++)
