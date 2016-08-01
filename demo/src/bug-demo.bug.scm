2ea78ded0ef498caf7a5678ae0c0a23eecf4ccd9
William Emerison Six <billsix@gmail.com>
2016-07-31 20:43:08 -0400

    The demo now does computation at compile-time.

--- demo/src/bug-demo.bug.scm
+++ demo/src/bug-demo.bug.scm
@@ -1,1 +1,10 @@
-(pp "foo")
+;; Copyright 2016 - William Emerison Six
+;;  All rights reserved
+;;  Distributed under LGPL 2.1 or Apache 2.0
+
+(##include "config.scm")
+
+{at-both-times
+ (pp "FIRST 10 PRIMES")
+ (pp (stream->list
+      (stream-take 10 primes)))}
