456f98c9142e2a387facb8f246b1a2e1f456e389
William Emerison Six <billsix@gmail.com>
2018-09-15 20:07:06 -0500

    fix bug demo

--- demo/src/bugdemo.scm
+++ demo/src/bugdemo.scm
@@ -7,21 +7,23 @@
 ;; "define" is namespaced in "config.scm"
 ;; It will evaluate definitions both at
 ;; compile time and at runtime.
-{define first-ten-primes
+(define first-ten-primes
   (stream->list
-   (stream-take 10 primes))}
+   (stream-take 10 primes)))
 
 ;; "unit-test" runs at compile-time
-{unit-test
+(unit-test
  (equal? first-ten-primes
-         '(2 3 5 7 11 13 17 19 23 29))}
+         '(2 3 5 7 11 13 17 19 23 29)))
 
-{at-both-times
+(at-both-times
  (pp "FIRST 10 PRIMES")
- (pp first-ten-primes)}
+ (pp first-ten-primes))
 
 ;; "##define" is Gambit's normal "define",
 ;; which is not evaluated during compile-time.
-{##define foo (command-line)}
+(##define foo (command-line))
 (pp foo)
-(pp ((compose [|x| (+ x 1)] [|y| (* y 2)]) 10))
+(pp ((compose (lambda (x) (+ x 1))
+              (lambda (y) (* y 2)))
+     10))
